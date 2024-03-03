611de7c12d02158981f3d851bd6f7cc7aec39c19
Daivaaddia <dkfaddia@gmail.com>
2024-03-03 13:38:23 +1100

    user

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -33,10 +33,79 @@ watch(selectedID, (id) => {
 </script>
 
 <template>
-    {{ user }}
+<div class="userViewMain">
+    <div class="userContainer">
+        <div class="profilePicContainer">
+            
+        </div>  
+        <div class="userName">
+            Placeholder
+        </div>
+        <div class="userStatsContainer">
+            <div class="userFriends">
+                Friends:<br> <span class="stat">10</span>
+            </div>
+            <div class="userContributions">
+                Contributions:<br> <span class="stat">10</span>
+            </div>
+        </div>
+    </div>
+</div>
     
 </template>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+* {
+    font-family: "Press Start 2P", system-ui;
+    color: #F595CF;
+}
+
+.userViewMain {
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/waterBG.png');
+    background-size: cover; 
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.userContainer {
+    background-color: #2B2038;
+    outline: 6px solid #FFCAEA;
+    display: flex;
+    flex-direction: column;
+    padding: 50px;
+    max-height: 500px;
+}
+
+.userStatsContainer {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+}
+
+.userName {
+    font-size: 2rem;
+    padding-bottom: 50px;
+    text-align: center;
+}
+
+.userFriends, .userContributions {
+    font-size: 1rem;
+    width: 15em;
+    height: 7em;    
+    background-color: #43304B;
+    padding: 1em;
+    text-align: center;
+}
+
+.stat {
+    font-size: 1.5rem;
+}
 
 </style>
